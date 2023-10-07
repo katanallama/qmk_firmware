e8d46b0168f2190a108c4d099a81cc10b748b1fe
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-10-07 07:27:27 -0600

    tweak layout and trim

--- keyboards/lazydesigners/bolt/keymaps/katanallama/keymap.c
+++ keyboards/lazydesigners/bolt/keymaps/katanallama/keymap.c
@@ -26,27 +26,27 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-               QK_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-               CTRLTAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-               SC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,
-               MO(3),    KC_LGUI,                   KC_LALT, LT1_SPC,          LT2_SPC, LT2_SPC,                   KC_LALT, KC_DOWN, KC_RGHT
+               QK_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC,
+               CTRLTAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT,
+               SC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, SC_RSPC,
+               MO(3),    KC_LALT,                   KC_LGUI, LT1_SPC,     LT2_SPC, KC_LGUI,                   KC_RALT,  KC_DOWN, KC_RGHT
              ),
     [1] = LAYOUT(
-               QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
-               G(KC_5),  G(KC_4), G(KC_3), G(KC_2), G(KC_1), G(KC_GRV),                 KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_LBRC, KC_BSLS,
-               KC_NO,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,           KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-               KC_VOLD,  KC_MUTE,                   KC_VOLU, KC_TRNS,          KC_TRNS, KC_NO,                     KC_NO,   KC_NO,   KC_NO
+               KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                 KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_DEL,
+               G(KC_5),  G(KC_4), G(KC_3), G(KC_2), G(KC_1), G(KC_GRV),            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_LBRC, KC_RBRC,
+               KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+               KC_VOLD,  KC_MUTE,                   KC_VOLU, KC_TRNS,     KC_TRNS, KC_NO,                     KC_NO,    KC_NO,   KC_NO
              ),
     [2] = LAYOUT(
-               QK_GESC,  KC_NO,   KC_NO,   KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
-               KC_NO,    KC_NO,   KC_NO,   SCRECG,  KC_PSCR, SCREC,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_QUOT, KC_BSLS,
-               KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-               KC_VOLD,  KC_MUTE,                   KC_VOLU, KC_TRNS,          KC_TRNS, KC_NO,                     KC_NO,   KC_NO,   KC_NO
+               QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                 KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_DEL,
+               KC_NO,    KC_NO,   KC_NO,   SCRECG,  KC_PSCR, SCREC,                KC_NO,   KC_EQL,  KC_MINS, KC_LBRC,  KC_QUOT, KC_BSLS,
+               KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+               KC_VOLD,  KC_MUTE,                   KC_VOLU, KC_TRNS,     KC_TRNS, KC_NO,                     KC_NO,    KC_NO,   KC_NO
              ),
     [3] = LAYOUT(
-               QK_BOOT,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-               KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-               KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-               KC_NO,    KC_NO,                     KC_NO,   KC_NO,            KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO
+               QK_BOOT,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                KC_NO,   KC_7,    KC_8,    KC_9,     KC_NO,   KC_NO,
+               KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                KC_NO,   KC_4,    KC_5,    KC_6,     KC_NO,   KC_NO,
+               KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,     KC_NO,   KC_NO,
+               KC_NO,    KC_NO,                     KC_NO,   KC_NO,       KC_NO,                     KC_0,    KC_NO,    KC_NO,   KC_NO
              ),
 };
--- keyboards/lazydesigners/bolt/keymaps/katanallama/rules.mk
+++ keyboards/lazydesigners/bolt/keymaps/katanallama/rules.mk
@@ -1,2 +1,2 @@
-CAPS_WORD_ENABLE = yes      # Capitalize the following word
-SEND_STRING_ENABLE = yes    # Send string macros
+# CAPS_WORD_ENABLE = yes      # Capitalize the following word
+# SEND_STRING_ENABLE = yes    # Send string macros
